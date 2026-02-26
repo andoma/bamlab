0d864a855d9a19ddef602c2b6475429409a8ecd1
Andreas Smas <andreas@lonelycoder.com>
2026-02-26 16:55:47 +0100

    Something

---
+++ bam.js
@@ -0,0 +1,23 @@
+
+const version = 'testversion';
+
+const run_container = debootstrap({
+  sources: [
+    "http://archive.ubuntu.com/ubuntu noble main restricted universe",
+    "http://archive.ubuntu.com/ubuntu noble-updates main restricted universe",
+    "http://archive.ubuntu.com/ubuntu noble-security main restricted universe"
+  ]
+});
+
+const build_container = from(run_container);
+
+build_container.apt([
+  "make",
+  "ccache",
+  "build-essential"]);
+
+const stage = from(build_container);
+
+stage.build("mkdir /build && cd /workspace && gcc -o /build/test src/test.c");
+stage.verifyElf("/build/test");
+
